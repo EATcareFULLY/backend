f38c144fb75e15d389395b4e79c1ba31e4719ae5
puzmarta <marta00puz@gmail.com>
2024-10-22 00:44:07 +0200

    Added mock endpoints for scanning labels and product recommendations

--- src/main/java/com/eatcarefully/backend/controller/ProductController.java
+++ src/main/java/com/eatcarefully/backend/controller/ProductController.java
@@ -1,13 +1,15 @@
 package com.eatcarefully.backend.controller;
 
+import com.eatcarefully.backend.dto.ProductRecommendationDTO;
+import com.eatcarefully.backend.model.Product;
 import com.eatcarefully.backend.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/products")
@@ -23,4 +25,44 @@ public class ProductController {
 
         return productService.getProductDetailsByBarcode(barcode);
     }
+
+    // mock endpoint for recommendation system
+
+    @GetMapping("/{barcode}/recommend")
+    public ResponseEntity<?> getProductRecommendationByBarcode(@PathVariable String barcode){
+
+        Product product = productService.getProductByBarcode(barcode);
+
+        //mock products to recommend
+
+        if( product != null){
+
+            String testingPossumUrl = "https://i.pinimg.com/474x/84/6f/85/846f8591330851c3ba9e11c3ea1afaa8.jpg";
+            List<ProductRecommendationDTO> recommendations = List.of(
+                    new ProductRecommendationDTO("1", "Product01", "A", "Brand1", testingPossumUrl),
+                    new ProductRecommendationDTO("2", "Product02", "B", "Brand3", testingPossumUrl),
+                    new ProductRecommendationDTO("3", "Product03", "C", "Brand2", testingPossumUrl)
+            );
+
+            return ResponseEntity.ok(recommendations);
+
+            }
+
+        return ResponseEntity.notFound().build();
+
+        }
+
+    @PostMapping("/scan-label")
+    public ResponseEntity<?> getLabelEvaluation(@RequestBody Map<String, String> request){
+
+        String labelText = request.get("labelText");
+
+        String eval = labelText + ": Mock eval response";
+
+        return ResponseEntity.ok(eval);
+
+    }
+
+
+
 }
