11cc8df9248712aff8425630764c5c7b9880500e
Dawid Spałek <dawid.spalek@convista.com>
2024-10-29 18:43:39 +0100

    Use Tesseract data from classpath while creating bean

--- src/main/java/com/eatcarefully/backend/config/TesseractConfig.java
+++ src/main/java/com/eatcarefully/backend/config/TesseractConfig.java
@@ -2,10 +2,15 @@ package com.eatcarefully.backend.config;
 
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.StringUtils;
+
+import java.io.IOException;
 
 @Configuration
 public class TesseractConfig {
@@ -13,12 +18,26 @@ public class TesseractConfig {
     @Value("${tesseract.path}")
     private String tesseractPath;
 
+    private final ResourceLoader resourceLoader;
+
+    public TesseractConfig(ResourceLoader resourceLoader,
+                           @Value("${tesseract.path}") String tesseractPath) {
+        this.resourceLoader = resourceLoader;
+        this.tesseractPath = tesseractPath;
+    }
+
     @Bean
     public ITesseract tesseract() {
         ITesseract tesseract = new Tesseract();
 
         if (StringUtils.hasLength(tesseractPath)) {
-            tesseract.setDatapath(tesseractPath);
+            try {
+                Resource resource = resourceLoader.getResource(tesseractPath);
+                String absolutePath = resource.getFile().getAbsolutePath();
+                tesseract.setDatapath(absolutePath);
+            } catch (IOException e) {
+                throw new BeanCreationException("Failed to initialize Tesseract with tessdata located in resources", e);
+            }
         }
 
         return tesseract;
