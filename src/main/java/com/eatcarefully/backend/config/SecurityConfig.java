76beafc455576f52534b683ee3cc7ccc09cb77a0
Dawid Spałek <dawid.spalek@convista.com>
2024-10-29 19:05:37 +0100

    Add property to disable security for development purposes

--- src/main/java/com/eatcarefully/backend/config/SecurityConfig.java
+++ src/main/java/com/eatcarefully/backend/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,23 +18,33 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Value("${spring.security.enabled}")
+    private boolean securityEnabled;
+
     private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/test/hello", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(STATELESS)
-                );
-
+        if (securityEnabled) {
+            http
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(authz -> authz
+                            .requestMatchers("/test/hello", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                            .anyRequest().authenticated()
+                    )
+                    .oauth2ResourceServer(oauth2 -> oauth2
+                            .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
+                    )
+                    .sessionManagement(session -> session
+                            .sessionCreationPolicy(STATELESS)
+                    );
+        } else {
+            http
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(authz -> authz
+                            .anyRequest().permitAll()
+                    );
+        }
         return http.build();
     }
 }
