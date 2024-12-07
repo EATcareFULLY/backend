aef9d4c83c63de53a791ac0d783f218bfe49ed66
puzmarta <marta00puz@gmail.com>
2024-12-07 21:41:00 +0100

    Added ddl migration

--- src/main/resources/db/migration/V1__Drop_Achievement_Tables.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS achievement_progress;
-DROP TABLE IF EXISTS achievement_definition;
---
+++ src/main/resources/db/migration/V4__Insert_Preference_Names.sql
@@ -0,0 +1,2 @@
+INSERT INTO preference_name (id, name) VALUES (1,'Eggs'), (2,'Milk'), (3,'Nuts'), (4,'Vegetarian'), (5,'Organic');;
+
